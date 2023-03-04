c5d2bec574bca8e20bfe83a0d2ab52161fdcd498
Rashi <rashipandeyster@gmail.com>
2023-03-03 22:47:42 -0800

    commited phonenumber file without staging

--- phonenumber.py
+++ phonenumber.py
@@ -1,4 +1,6 @@
 dict = {"Rashi" : 234534, "Shail" : 34567 , "Anvay": 654787, "Nysa":9876543}
 print("Phone number and names are: ")
+count = 0
 for i,j in dict.items():
-    print(i, "=" , j)
+    count = count+1
+    print(count, i, "=" , j)
